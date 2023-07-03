0226f993d369f45830b004ac2c6272abd796bcd4
Adnan Arodiya <99405994+Adnanarodiya@users.noreply.github.com>
2023-07-03 15:27:26 +0530

    Create pr2a.php
    Find the given number is Odd or Even

---
+++ sem5/AWD/pr2/pr2a.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 68;
+    if($a % 2 == 0){
+        echo"$a is even";
+    }else{
+        echo"$a is odd";
+    }
+    ?>
+</body>
+</html>
